ba6725845f1528428f0a8c0ba015ebac28c9861e
lixworth <lixworth@gmail.com>
2020-02-03 22:30:10 +0800

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,47 @@
 /**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
-require('./bootstrap');
-
-window.Vue = require('vue');
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
+ * Amadeus Panel
+ * @link https://github.com/steinsmc/Amadeus
+ * @author LixWorth<lixworth@163.com>
  *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ * 这里使用了vuetifyjs(https://vuetifyjs.com) 构建前端页面
+ * 可以参考这个实例自行扩展
  */
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+// require('./bootstrap');
+window.Vue = require('vue');
+import Vuetify from 'vuetify';
+import Vuex from 'vuex'
+
+console.log("\n" +
+    " ______                              __                             \n" +
+    "/\\  _  \\                            /\\ \\                            \n" +
+    "\\ \\ \\L\\ \\     ___ ___       __      \\_\\ \\      __   __  __    ____  \n" +
+    " \\ \\  __ \\  /' __` __`\\   /'__`\\    /'_` \\   /'__`\\/\\ \\/\\ \\  /',__\\ \n" +
+    "  \\ \\ \\/\\ \\ /\\ \\/\\ \\/\\ \\ /\\ \\L\\.\\_ /\\ \\L\\ \\ /\\  __/\\ \\ \\_\\ \\/\\__, `\\\n" +
+    "   \\ \\_\\ \\_\\\\ \\_\\ \\_\\ \\_\\\\ \\__/.\\_\\\\ \\___,_\\\\ \\____\\\\ \\____/\\/\\____/\n" +
+    "    \\/_/\\/_/ \\/_/\\/_/\\/_/ \\/__/\\/_/ \\/__,_ / \\/____/ \\/___/  \\/___/ \n" +
+    "                                                                    \n" +
+    " Amadeus Panel Version: "+document.getElementsByTagName('meta')['amadeus'].content+" github: https://github.com/steinsmc ");
+
+Vue.use(Vuetify);
+Vue.use(Vuex);
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('login',require('./components/Login').default);
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+const store = new Vuex.Store({
+    state: {
+    },
+    mutations: {
+    },
+    actions: {
+
+    }
+});
 
 const app = new Vue({
-    el: '#app',
+    el: '#amadeus-bugdhdj-app', //BUGDHDJ 保佑无需debug
+    store,
+    vuetify: new Vuetify({}),
 });
+
--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,5 @@ Route::get('/', function () {
 
 
 Route::get('/login',function (){
-
+    return view("login");
 });
